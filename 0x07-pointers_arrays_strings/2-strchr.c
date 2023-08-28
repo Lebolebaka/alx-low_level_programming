5c257eb5dd2167eaf329c7942ef0d612250c1111
moleboheng <molebohenglebaka115@gmail.com>
2023-08-28 13:43:44 -0700

    pointers and arrays

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strchr - prints from the first occurrence of a char.
+ * @s: source string
+ * @c: tested char
+ *
+ * Return: new string.
+ */
+char *_strchr(char *st, char c)
+{
+	while (*st != '\0')
+	{
+		if (*st == c)
+		{
+			return (st);
+
+		}
+		else
+			s++;
+	}
+	if (*st == c)
+		return (st);
+	else
+		return (0);
+}
