e9981169127ed9d0bf8e068217a0c2aeec5010b1
moleboheng <molebohenglebaka115@gmail.com>
2023-09-06 13:17:21 -0700

    static libraries

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,17 +1,19 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include "holberton.h"
 
 /**
- * main - function to print file name
- * @argc: count of the arguments supplied to the program and the
- * @argv: an array of pointers to the strings
- * Return: Always 0
+ * main - Write a program that prints its name, followed by a new line.
+ * If you rename the program, it will print the new name, without having to
+ * compile it again
+ * You should not remove the path before the name of the program
+ *
+ * @argc: This is the argument count
+ * @argv: This is the argument vector
+ *
+ * Return: This return to 0
  */
-
 int main(int argc, char *argv[])
 {
-	if (argc > 0)
-		printf("%s\n", argv[0]);
+	printf("%s\n", argv[0]);
+	(void)argc;
 	return (0);
 }
